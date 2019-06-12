aa4d7671b6db5cefc5d45aa5f2662fce87938e10
Alan Casas <alancasasarevalo@gmail.com>
2019-06-12 17:50:06 +0200

    All routes were protected by auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,19 @@
 const passport = require('passport');
 const _constants = require('../src/config/constants');
 
+// Funcion para autenticar el usuario
 exports.userAuthentication = passport.authenticate(_constants.LITERALS_AUTHENTICATION_CONTROLLER.AUTHENTICATION_STRATEGY, {
     successRedirect: _constants.LITERALS_AUTHENTICATION_CONTROLLER.AUTHENTICATION_SUCCESS_REDIRECT_URL,
     failureRedirect: _constants.LITERALS_AUTHENTICATION_CONTROLLER.AUTHENTICATION_FAILURE_REDIRECT_URL,
     failureFlash: true,
     badRequestMessage: _constants.LITERALS_AUTHENTICATION_CONTROLLER.BAD_REQUEST_MESSAGE
 });
+
+// Funcion para revisar si el usuario esta logeado
+exports.userAuthenticaticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        return res.redirect(_constants.LITERALS_AUTHENTICATION_CONTROLLER.POST_AUTHENTICATION);
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -7,48 +7,65 @@ const usersControllers = require('../controllers/usersControllers');
 const authController = require('../controllers/authController');
 const _constants = require('../src/config/constants');
 
-
 module.exports = function () {
 
-    router.get(_constants.INDEX_ROUTES_LITERALS.GET_PROJECTS_HOME, projectsController.projectsHome);
+    router.get(_constants.INDEX_ROUTES_LITERALS.GET_PROJECTS_HOME,
+        authController.userAuthenticaticated,
+        projectsController.projectsHome
+    );
 
-    router.get(_constants.INDEX_ROUTES_LITERALS.GET_POST_NEW_PROJECTS, projectsController.projectsNewProjects);
+    router.get(_constants.INDEX_ROUTES_LITERALS.GET_POST_NEW_PROJECTS,
+        authController.userAuthenticaticated,
+        projectsController.projectsNewProjects
+    );
 
     router.post(_constants.INDEX_ROUTES_LITERALS.GET_POST_NEW_PROJECTS,
         //Validacion de nombre, que no este vacio, que elimine los espacios al principio y final y que escape los caracteres extraños
+        authController.userAuthenticaticated,
         body('name').not().isEmpty().trim().escape(),
         projectsController.newProject
     );
 
     // Lista de projectos
-    router.get(_constants.INDEX_ROUTES_LITERALS.GET_PROJECTS_BY_URL, projectsController.projectByUrl);
+    router.get(_constants.INDEX_ROUTES_LITERALS.GET_PROJECTS_BY_URL,
+        authController.userAuthenticaticated,
+        projectsController.projectByUrl
+    );
 
-    router.get(_constants.INDEX_ROUTES_LITERALS.GET_EDIT_PROJECTS_BY_ID, projectsController.editForm);
+    router.get(_constants.INDEX_ROUTES_LITERALS.GET_EDIT_PROJECTS_BY_ID,
+        authController.userAuthenticaticated,
+        projectsController.editForm
+    );
 
     router.post(_constants.INDEX_ROUTES_LITERALS.POST_PROJECTS_BY_ID,
+        authController.userAuthenticaticated,
         body('name').not().isEmpty().trim().escape(),
         projectsController.updateProjects
     );
 
     // Eliminar proyecto
     router.delete(_constants.INDEX_ROUTES_LITERALS.DELETE_PROJECTS_BY_URL,
+        authController.userAuthenticaticated,
         projectsController.deleteProjects
     );
 
     //TASKS
     // Crear nueva tarea
     router.post(_constants.INDEX_ROUTES_LITERALS.POST_NEW_TASK_BY_PROJECT_URL,
+        authController.userAuthenticaticated,
         tasksController.addNewTask
     );
 
     //Actualizar tarea task
     // PUT reescribe el elemento que queremos actualizar mientras que path solo cambia una propiedad del elemento
     router.patch(_constants.INDEX_ROUTES_LITERALS.PATCH_TASK_BY_ID,
+        authController.userAuthenticaticated,
         tasksController.updateTask
     );
 
     //Borrar tarea task
     router.delete( _constants.INDEX_ROUTES_LITERALS.DELETE_TASK_BY_ID,
+        authController.userAuthenticaticated,
         tasksController.deleteTask
     );
 
